f4f15d0ea3156c988302010ad88ed1f4cf27ca72
pgeseraphin <pgeseraphin@73e4faf7-30dd-42b8-f396-fc12991c0e65>
2010-12-24 18:17:14 +0000

    Ajout dans la partie administrative du site : * Ajout des repertoires style, css, img et js (et ajout de fichiers a l'interieur de ces repertoires) * Ajout du fichier db.php pour la connexion avec la base de donnees * Modification du fichier admin.php

---  bbsqli/admin/db.php
+++  bbsqli/admin/db.php
@@ -1,9 +1,10 @@
 <?php
 /*
- * Created on Dec 9, 2010
+ * Created on Dec 24, 2010
  *
  * To change the template for this generated file go to
  * Window - Preferences - PHPeclipse - PHP - Code Templates
- */ 
- phpinfo();
+ */
+ 
+ $conn = new mysqli('localhost','root','tololo','Blog');
 ?>
