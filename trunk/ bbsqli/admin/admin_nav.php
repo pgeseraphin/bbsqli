7ce5f587a5e455942e7c474bd51c2e19ef0f7ee1
pgeseraphin <pgeseraphin@73e4faf7-30dd-42b8-f396-fc12991c0e65>
2011-01-05 13:06:08 +0000

    * Modification de la navigation pour les administrateurs et les moderateurs

--- trunk/ bbsqli/admin/admin_nav.php
+++ trunk/ bbsqli/admin/admin_nav.php
@@ -8,23 +8,23 @@
  */
 
 echo '<ul id="mainNav">' .
-'<li><a href= "index.php" class="active">ACCUEIL</a></li>' .
+'<li><a href= "../index.php" class="active">ACCUEIL</a></li>' .
 '<!-- Use the "active" class for the active menu item  -->' .
 '<li><a href="#">OPTION</a></li>';
 
 session_start();
 
 if ($_SESSION['user_type'] == '1') {
-	echo '<li><a href="admin/admin.php">ADMINISTRATION</a></li>';
+	echo '<li><a href="admin.php">ADMINISTRATION</a></li>';
 }
 elseif ($_SESSION['user_type'] == '2') {
-	echo '<li><a href="admin/moder.php">MODERATION</a></li>';
+	echo '<li><a href="moder.php">MODERATION</a></li>';
 }
 
 if (!isset($_SESSION['user_id'])) {
-	echo '<li class="logout"><a href = "connexion.php">CONNEXION</a>';
+	echo '<li class="logout"><a href = "../connexion.php">CONNEXION</a>';
 } else {
-	echo '<li class="logout"><a href = "deconnexion_post.php">DECONNEXION</a>';
+	echo '<li class="logout"><a href = "../deconnexion_post.php">DECONNEXION</a>';
 }
 
 
--- trunk/ bbsqli/deconnexion_post.php
+++ trunk/ bbsqli/deconnexion_post.php
@@ -1,14 +1,20 @@
 <?php
+
 /*
  * Created on 3 janv. 2011
  *
  * To change the template for this generated file go to
  * Window - Preferences - PHPeclipse - PHP - Code Templates
  */
- session_start();
- //on écrase le tableau de session
- $_SESSION = array();
- // on détruit la session
- session_destroy();
- header('Location: index.php');
+session_start();
+//on écrase le tableau de session
+//$_SESSION = array ();
+// on détruit la session
+//session_destroy();
+
+unset ($_SESSION['user_id']);
+unset ($_SESSION['user_login']);
+unset ($_SESSION['user_type']);
+
+header('Location: index.php');
 ?>
