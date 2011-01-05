25bcf9cc21923b13eddb72c7d558e7c0970102a3
pgeseraphin <pgeseraphin@73e4faf7-30dd-42b8-f396-fc12991c0e65>
2011-01-05 11:48:11 +0000

    * Modification de header

---  bbsqli/header.php
+++  bbsqli/header.php
@@ -6,11 +6,11 @@
 <title>Mon Blog - Profil</title>
 
 <!-- CSS -->
-<link href="admin/style/css/transdmin.css" rel="stylesheet" type="text/css" media="screen" />
+<link href="lib/template/style/css/transdmin.css" rel="stylesheet" type="text/css" media="screen" />
 <!--[if IE 6]><link rel="stylesheet" type="text/css" media="screen" href="admin/style/css/ie6.css" /><![endif]-->
 <!--[if IE 7]><link rel="stylesheet" type="text/css" media="screen" href="admin/style/css/ie7.css" /><![endif]-->
 
 
 <!-- JavaScripts-->
-<script type="text/javascript" src="admin/style/js/jquery.js"></script>
-<script type="text/javascript" src="admin/style/js/jNice.js"></script>
+<script type="text/javascript" src="lib/template/style/js/jquery.js"></script>
+<script type="text/javascript" src="lib/template/style/js/jNice.js"></script>
