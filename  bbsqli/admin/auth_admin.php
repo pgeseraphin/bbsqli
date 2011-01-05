afbca8a5923505adb7919cb6b178c106712b9de9
pgeseraphin <pgeseraphin@73e4faf7-30dd-42b8-f396-fc12991c0e65>
2011-01-05 11:15:48 +0000

    * Modification de l'authentification dans admin

---  bbsqli/admin/auth_admin.php
+++  bbsqli/admin/auth_admin.php
@@ -1,4 +1,4 @@
 <?php
   session_start();
-  if ($_SESSION['user_type']!='1') header('Location: http://'. $_SERVER['HTTP_HOST'] . '/connexion.php');
+  if ($_SESSION['user_type']!='1') header('Location: http://'. $_SERVER['HTTP_HOST'] . '/bbsqli/connexion.php');
 ?>
