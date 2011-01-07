8b131d4dd02aa26c6a68633ee6ac306b41032b12
saliou06@hotmail.fr <saliou06@hotmail.fr@73e4faf7-30dd-42b8-f396-fc12991c0e65>
2011-01-07 15:23:43 +0000

    masquer le lien vers le profil quand on est pas connecté

--- trunk/ bbsqli/menu.php
+++ trunk/ bbsqli/menu.php
@@ -3,8 +3,12 @@
          <li><a href="inscription.php">S'inscrire</a></li>                	    
          <li><a href="#">Céer un blog</a></li>                    	
          <li><a href="#">Archives</a></li>
-         <li><a href="user_profil.php">Mon profil</a></li>
-                    	
+         <?php
+          session_start();
+          if (isset($_SESSION['user_id'])) 
+          	echo '<li><a href="user_profil.php">Mon profil</a></li>';
+         ?>
+                             	
     </ul>
       <!-- // .sideNav -->
 </div>   
