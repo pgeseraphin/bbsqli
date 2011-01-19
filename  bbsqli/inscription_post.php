5a98f4327ea9816576c203cb0bf47ebf66710ace
saliou06@hotmail.fr <saliou06@hotmail.fr@73e4faf7-30dd-42b8-f396-fc12991c0e65>
2011-01-19 14:41:31 +0000

    correction de bugs dans les formulaires

---  bbsqli/inscription_post.php
+++  bbsqli/inscription_post.php
@@ -18,16 +18,16 @@ if(empty($_REQUEST['nom']) || empty($_REQUEST['prenom']) ||
 	header('Location: inscription.php');
 	//die("ERREUR : les champs obligatoires doivent être remplis.");
 }
-
 $sql = "INSERT INTO Utilisateur(Login, Password, Email, TitreBlog, Prenom, Nom, Sexe, DateNaissance, Adresse, CodePostal, Ville, Pays, Telephone, Mobile, Fax, AproposDeMoi) VALUES (' ".$_REQUEST['login']."', ' ".$_REQUEST['mot_de_passe']."', ' ".$_REQUEST['email']."', ' ".$_REQUEST['titre_blog']."', ' ".$_REQUEST['prenom']."', ' ".$_REQUEST['nom']."', ' ".$_REQUEST['choix']."', ' ".$_REQUEST['date_de_naissance']."', ' ".$_REQUEST['adresse']."', ' ".$_REQUEST['code_postal']."', ' ".$_REQUEST['ville']."', ' ".$_REQUEST['pays']."', ' ".$_REQUEST['tel_fixe']."', ' ".$_REQUEST['mobile']."', ' ".$_REQUEST['fax']."',  ' ".$_REQUEST['description']."')";
-
+//$sql = "INSERT INTO Utilisateur(Login, Password, Email, TitreBlog, Prenom, Nom, Sexe, DateNaissance, Adresse, CodePostal, Ville, Pays, Telephone, Mobile, Fax, AproposDeMoi) VALUES ('$_REQUEST['login']', '$_REQUEST['mot_de_passe']', '$_REQUEST['email']', '$_REQUEST['titre_blog']', '$_REQUEST['prenom']', '$_REQUEST['nom']', '$_REQUEST['choix']', '$_REQUEST['date_de_naissance']', '$_REQUEST['adresse']', '$_REQUEST['code_postal']', '$_REQUEST['ville']', '$_REQUEST['pays']', '$_REQUEST['tel_fixe']', '$_REQUEST['mobile']', '$_REQUEST['fax']',  '$_REQUEST['description']')";
+//$sql = "INSERT INTO Utilisateur(Login, Password, Email) VALUES ('$_REQUEST['login']', '$_REQUEST['mot_de_passe']', '$_REQUEST['email']')";
 if($conn->query($sql)){
-	print(" enregistrement avec succès");
+	//print(" enregistrement avec succès");
 	header('Location: index.php');
 	
 }
 else {
-	print("Echec de création ");
+	//print("Echec de création ");
 	header('Location: inscription.php');
 }
 $conn->close();
---
+++  bbsqli/test_connexion_escape_string.php
@@ -0,0 +1,100 @@
+<?php
+
+
+/*
+ * Created on 2 janv. 2011
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+session_start();
+require_once 'param_connexion.php';
+require_once 'header.php';
+?>
+  
+</head>
+<body>
+
+<div id="wrapper">
+    	
+    	<h1><a href="#"><span>Mon Blog</span></a></h1>
+        
+        <!-- You can name the links with lowercase, they will be transformed to uppercase by CSS, we prefered to name them with uppercase to have the same effect with disabled stylesheet -->
+       <?php
+
+
+include ("navigation.php");
+?>
+        <!-- // #end mainNav -->
+        
+        <div id="containerHolder">
+			<div id="container">
+        		<div id="sidebar">  
+        		    <ul class="sideNav">         
+         <li><a href="test_connexion.php">Authentification</a></li>
+         <li><a href="#">Authent. escape string </a></li>     
+                            	
+    </ul>              	
+                    <!-- // .sideNav -->
+                </div>  
+                  
+                <!-- // #sidebar -->                            
+                
+                <div id="main">
+                <form action="" method="post" class="jNice">
+                	<h3>Paramètres d'authentification utilisant l'échappement de caractères spéciaux'</h3>
+                    	<fieldset>
+                        	<p><label>Login:</label><input type="text" name="login" class="text-long" /></p>
+                        	<p><label>Mot de passe:</label><input type="password" name="mot_de_passe" class="text-long" /></p>                        	
+                        	<p><a href= "">mot de passe oublie? </a></p>
+                        	<input type="submit" value="Se connecter" />
+                        </fieldset>
+                    </form>             
+                                  
+                
+                <?php
+
+
+//$valide = (!empty ($_REQUEST['login']) AND !empty ($_REQUEST['mot_de_passe']));
+$valide = (isset ($_REQUEST['login']) AND isset ($_REQUEST['mot_de_passe']) AND !empty($_REQUEST['login']) AND !empty($_REQUEST['mot_de_passe']));
+
+if ($valide) {
+	
+	$login = $conn->real_escape_string($_REQUEST['login']);
+    $pwd = $conn->real_escape_string($_REQUEST['mot_de_passe']);
+	$sql = "SELECT IdUtilisateur, Login, Type, Email FROM Utilisateur WHERE Login = '$login' AND Password = '$pwd'";	
+	$resultat = $conn->query($sql);
+	if ($resultat->num_rows > 0) {
+		
+		$obj = $resultat->fetch_object();
+		$_SESSION['user_id'] = $obj->IdUtilisateur;
+		$_SESSION['user_login'] = $obj->Login;
+		$_SESSION['user_type'] = $obj->Type;		
+
+		header('Location: user_profil.php');
+	} else {
+
+		//echo " Echec d'authentification ";
+		echo '<span style="color : red;">Echec d\'authentification</span>';
+		//header('Location: test_connexion.php');
+	}
+	$resultat->close();
+	$conn->close();
+
+}
+?>
+</div>
+                <!-- // #main -->
+                
+                <div class="clear"></div>
+            </div>
+            <!-- // #container -->
+        </div>	
+        <!-- // #containerHolder -->
+        
+        <?php require_once 'footer.php'; ?>
+    </div>
+    <!-- // #wrapper --> 	
+</body>
+</html>
+
