99f1426082d733d88adca439cd6b661cdf42f96c
saliou06@hotmail.fr <saliou06@hotmail.fr@73e4faf7-30dd-42b8-f396-fc12991c0e65>
2011-01-26 15:07:47 +0000

    add news tests

---  bbsqli/menu.php
+++  bbsqli/menu.php
@@ -4,9 +4,11 @@
          <li><a href="#">Céer un blog</a></li>                    	
          <li><a href="#">Archives</a></li>
          <li><a title= "Post->String: sans protection" href="test_connexion.php">Authentification [string]</a></li>
-         <li><a title= "Post->String: avec protection" href="test_connexion_escape_string.php">Authent. [string]+escape  </a></li>
+         <li><a title= "Post->String: avec protection" href="test_connexion_escape_string.php">Authent. [string]+Escape  </a></li>
          <li><a title= "Post->Int: sans protection" href="test_connexion_int.php">Authent. [int]</a></li>
-         <li><a title= "Post->Int: avec escape" href="test_connexion_int_fixed1.php">Authent. [int]+escape </a></li>
+         <li><a title= "Post->Int: avec Escape" href="test_connexion_int_fixed1.php">Authent. [int]+Escape </a></li>
+         <li><a title= "Post->Int: avec Escape+Guillemet" href="test_connexion_int_fixed2.php">Authent. [int]+Guillemet </a></li>
+         <li><a title= "Post->Int: avec Escape+Guillemet+Inval" href="test_connexion_int_fixed3.php">Authent. [int]+Inval </a></li>
          <li><a href="test_findUser.php">Trouver un user 1</a></li>
          <li><a href="test_findUser_escape_string.php">Trouver un user 2</a></li>
          <li><a href="test_search.php">Trouver un Blog 1</a></li>
