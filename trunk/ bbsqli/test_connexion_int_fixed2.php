f62a976703a680973aa852af26cd046253339c8b
saliou06@hotmail.fr <saliou06@hotmail.fr@73e4faf7-30dd-42b8-f396-fc12991c0e65>
2011-01-26 15:14:14 +0000

    add some links in the menu

--- trunk/ bbsqli/test_connexion_int_fixed2.php
+++ trunk/ bbsqli/test_connexion_int_fixed2.php
@@ -31,8 +31,12 @@ include ("navigation.php");
 			<div id="container">
         		<div id="sidebar">  
         		    <ul class="sideNav">         
-         <li><a href="test_connexion.php">Authentification</a></li>
-         <li><a href="test_connexion_escape_string.php">Authent. escape string </a></li>     
+         <li><a title= "Post->String: sans protection" href="test_connexion.php">Authentification [string]</a></li>
+         <li><a title= "Post->String: avec protection" href="test_connexion_escape_string.php">Authent. [string]+Escape  </a></li>
+         <li><a title= "Post->Int: sans protection" href="test_connexion_int.php">Authent. [int]</a></li>
+         <li><a title= "Post->Int: avec Escape" href="test_connexion_int_fixed1.php">Authent. [int]+Escape </a></li>
+         <li><a title= "Post->Int: avec Escape+Guillemet" href="test_connexion_int_fixed2.php">Authent. [int]+Guillemet </a></li>
+         <li><a title= "Post->Int: avec Escape+Guillemet+Inval" href="test_connexion_int_fixed3.php">Authent. [int]+Inval </a></li>
                             	
     </ul>              	
                     <!-- // .sideNav -->
