f11b9489e1423068f1efd529d3a12275833062ea
saliou06@hotmail.fr <saliou06@hotmail.fr@73e4faf7-30dd-42b8-f396-fc12991c0e65>
2011-01-28 17:24:42 +0000

    Ajout de test sur la clause Limit avec inval/is_numeric

--- trunk/ bbsqli/menu.php
+++ trunk/ bbsqli/menu.php
@@ -8,11 +8,12 @@
          <li><a title= "Post->Int: sans protection" href="test_connexion_int.php">Authent. [int]</a></li>
          <li><a title= "Post->Int: avec Escape" href="test_connexion_int_fixed1.php">Authent. [int]+Escape </a></li>
          <li><a title= "Post->Int: avec Escape+Guillemet" href="test_connexion_int_fixed2.php">Authent. [int]+Guillemet </a></li>
-         <li><a title= "Post->Int: avec Escape+Guillemet+Inval" href="test_connexion_int_fixed3.php">Authent. [int]+Inval </a></li>
+         <li><a title= "Post->Int: avec Escape+Guillemet+Intval" href="test_connexion_int_fixed3.php">Authent. [int]+Inval </a></li>
          <li><a href="test_findUser.php">Trouver un user 1</a></li>
          <li><a href="test_findUser_escape_string.php">Trouver un user 2</a></li>
-         <li><a href="test_search.php">Trouver un Blog 1</a></li>
-         <li><a href="test_search_fixed.php">Trouver un Blog 2</a></li>
+         <li><a title= "Post-> Limit: sans protection" href="test_search.php">Liste des User 1</a></li>
+         <li><a title= "Post-> Limit: Guillemet+Intval" href="test_search_fixed1.php">Liste des User 2</a></li>
+         <li><a title= "Post-> Limit: is_numeric" href="test_search_fixed2.php">Liste des User 3</a></li>
          <?php
           session_start();
           if (isset($_SESSION['user_id'])) 
