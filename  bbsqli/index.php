2f7101e7f55003a84cca87b726ad18c4aa0bac62
wisner.cazeau <wisner.cazeau@73e4faf7-30dd-42b8-f396-fc12991c0e65>
2011-02-17 13:07:47 +0000

    ajout wisner dans le commentaire

---  bbsqli/index.php
+++  bbsqli/index.php
@@ -7,6 +7,7 @@
  * To change the template for this generated file go to
  * Window - Preferences - PHPeclipse - PHP - Code Templates
  * test
+ *Wisner
  */
 session_start();
 require_once 'param_connexion.php';
