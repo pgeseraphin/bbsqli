aeeb1654a41554433f97ed87ff1382bcce3f8e23
saliou06@hotmail.fr <saliou06@hotmail.fr@73e4faf7-30dd-42b8-f396-fc12991c0e65>
2011-02-17 15:27:33 +0000

    ajout de test pour Like

--- trunk/ bbsqli/menu.php
+++ trunk/ bbsqli/menu.php
@@ -9,9 +9,10 @@
          <li><a title= "Post->Int: avec Escape" href="test_connexion_int_fixed1.php">Authent. [int]+Escape </a></li>
          <li><a title= "Post->Int: avec Escape+Guillemet" href="test_connexion_int_fixed2.php">Authent. [int]+Guillemet </a></li>
          <li><a title= "Post->Int: avec Escape+Guillemet+Intval" href="test_connexion_int_fixed3.php">Authent. [int]+Inval </a></li>
-         <li><a href="test_findUser.php">Trouver un user 1</a></li>
-         <li><a href="test_findUser_escape_string.php">Trouver un user 2</a></li>
-         <li><a title= "Post-> Limit: sans protection" href="test_search.php">Liste des Users 1</a></li>
+         <li><a title= "Post-> Like: sans protection" href="test_findUser.php">Trouver un user 1</a></li>
+         <li><a title= "Post-> Like: Escape" href="test_findUser_escape_string.php">Trouver un user 2</a></li>
+         <li><a title= "Post-> Like: Escape+addcslashes" href="test_findUser_addcslashes.php">Trouver un user 3</a></li>
+         <li><a title= "Post-> Limit: Escape" href="test_search.php">Liste des Users 1</a></li>
          <li><a title= "Post-> Limit: Guillemet+Intval" href="test_search_fixed1.php">Liste des Users 2</a></li>
          <li><a title= "Post-> Limit: is_numeric" href="test_search_fixed2.php">Liste des Users 3</a></li>
          <li><a title= "Post-> Order by: + Escape" href="test_order_by.php">Liste des Users 4</a></li>
