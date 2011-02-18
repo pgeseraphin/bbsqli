c8853e9ab3c36b9e94d847e339b1ff9a116be754
pgeseraphin <pgeseraphin@73e4faf7-30dd-42b8-f396-fc12991c0e65>
2011-02-18 14:14:22 +0000

    * Ajout de cookie_string

---
+++  bbsqli/admin/cookie_string/admin_side.php
@@ -0,0 +1,10 @@
+<div id="sidebar">
+	<ul class="sideNav">
+    	<li><a href="../admin.php?cat=3" <?php if($active==3) echo " class=active"; ?>>Utilisateurs</a></li>
+    	<li><a href="../admin.php?cat=2" <?php if($active==2) echo " class=active"; ?>>Mod&eacute;rateurs</a></li>
+    	<li><a href="../admin.php?cat=1" <?php if($active==1) echo " class=active"; ?>>Administrateurs</a></li>
+    	<li><a href="../admin_profil.php" <?php if($active==4) echo " class=active"; ?>>Mon Profil</a></li>                    	
+    </ul>
+    <!-- // .sideNav -->
+</div>    
+<!-- // #sidebar -->
