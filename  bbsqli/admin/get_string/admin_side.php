db583f09d471206220a333a0a211fe62994b2f2e
pgeseraphin <pgeseraphin@73e4faf7-30dd-42b8-f396-fc12991c0e65>
2011-02-27 13:48:17 +0000

    * Modification de variables GET

---  bbsqli/admin/get_string/admin_side.php
+++  bbsqli/admin/get_string/admin_side.php
@@ -1,12 +1,12 @@
 <div id="sidebar">
 	<ul class="sideNav">
-    	<li><a href="test_string_GET.php?login=">Sans protection</a></li>
-    	<li><a href="test_string_escape.php?login=">Escape</a></li>
-    	<li><a href="test_string_escape_quote.php?login=">Escape + Guillemet</a></li>
-    	<li><a href="test_string_order_by.php?login=">ORDER BY</a></li>
-    	<li><a href="test_string_order_by_quote.php?login=">ORDER BY + quote</a></li>
-    	<li><a href="test_string_like_escape.php?login=">LIKE + escape</a></li>
-    	<li><a href="test_string_like_escape_quote.php?login=cazeau">LIKE + escape + quote</a></li>
+    	<li><a href="test_string_GET.php?login=pgeseraphin">Sans protection</a></li>
+    	<li><a href="test_string_escape.php?login=pgeseraphin">Escape</a></li>
+    	<li><a href="test_string_escape_quote.php?login=pgeseraphin">Escape + Guillemet</a></li>
+    	<li><a href="test_string_order_by.php?login=Login">ORDER BY</a></li>
+    	<li><a href="test_string_order_by_quote.php?login=Login">ORDER BY + quote</a></li>
+    	<li><a href="test_string_like_escape.php?login=pge">LIKE + escape</a></li>
+    	<li><a href="test_string_like_escape_quote.php?login=pge">LIKE + escape + quote</a></li>
     	</ul>
     <!-- // .sideNav -->
 </div>    
---  bbsqli/admin/get_string_erreur/admin_side.php
+++  bbsqli/admin/get_string_erreur/admin_side.php
@@ -1,8 +1,8 @@
 <div id="sidebar">
 	<ul class="sideNav">
-    	<li><a href="test_string_GET.php?login=">Sans protection</a></li>
-    	<li><a href="test_string_escape.php?login=">Escape</a></li>
-    	<li><a href="test_string_escape_quote.php?login=">Escape + Guillemet</a></li>
+    	<li><a href="test_string_GET.php?login=pgeseraphin">Sans protection</a></li>
+    	<li><a href="test_string_escape.php?login=pgeseraphin">Escape</a></li>
+    	<li><a href="test_string_escape_quote.php?login=pgeseraphin">Escape + Guillemet</a></li>
     	 </ul>
     <!-- // .sideNav -->
 </div>    
