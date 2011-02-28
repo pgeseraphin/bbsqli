cf47e101059d6b54149662fe22c58d8634697930
pgeseraphin <pgeseraphin@73e4faf7-30dd-42b8-f396-fc12991c0e65>
2011-02-28 17:21:53 +0000

    * Implémentation des tests pour cookie string avec affichage d'erreur

---
+++ trunk/ bbsqli/admin/cookie_string_erreur/admin_nav.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * Created on 5 janv. 2011
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+
+echo '<ul id="mainNav">' .
+'<li><a href= "../../index.php" class="active">ACCUEIL</a></li>' .
+'<!-- Use the "active" class for the active menu item  -->' .
+'<li><a href="../../test_get.php">GET</a></li>' .
+'<li><a href="../../test_post.php">POST</a></li>' .
+'<li><a href="../../test_cookie.php">COOKIE</a></li>';
+
+session_start();
+
+if ($_SESSION['user_type'] == '1') {
+	echo '<li><a href="admin.php">ADMINISTRATION</a></li>';
+}
+elseif ($_SESSION['user_type'] == '2') {
+	echo '<li><a href="moder.php">MODERATION</a></li>';
+}
+
+if (!isset($_SESSION['user_id'])) {
+	echo '<li class="logout"><a href = "../connexion.php">CONNEXION</a>';
+} else {
+	echo '<li class="logout"><a href = "../deconnexion_post.php">DECONNEXION</a>';
+}
+
+
+echo '</li></ul>';
+?>
--- trunk/ bbsqli/test_cookie.php
+++ trunk/ bbsqli/test_cookie.php
@@ -25,7 +25,7 @@ require_once 'nav_cookie.php';
         		     <li><a href="admin/cookie/test_COOKIE_int.php">COOKIE INT (sans erreur)</a></li>
         		     <li><a href="admin/cookie_erreur/test_COOKIE_int_erreur.php">COOKIE INT (avec erreur)</a></li>         		     
         		    <li><a href="admin/cookie_string/test_COOKIE_string_.php">COOKIE STR (sans err.)</a></li>
-        		     <li><a href="#">COOKIE STR (avec err.)</a></li>         		     
+        		     <li><a href="admin/cookie_string_erreur/test_COOKIE_string_erreur.php">COOKIE STR (avec err.)</a></li>         		     
         		   </ul>              	
                     <!-- // .sideNav -->
                 </div>  
