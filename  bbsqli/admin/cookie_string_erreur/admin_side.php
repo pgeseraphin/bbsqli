409bcc8feb7e38d7210fe5421743b0081cd427ce
pgeseraphin <pgeseraphin@73e4faf7-30dd-42b8-f396-fc12991c0e65>
2011-02-28 17:21:53 +0000

    * Implémentation des tests pour cookie string avec affichage d'erreur

---
+++  bbsqli/admin/cookie_string_erreur/admin_side.php
@@ -0,0 +1,9 @@
+<div id="sidebar">
+	<ul class="sideNav">
+    	<li><a href="test_unprotect.php">Sans protection</a></li>
+    	<li><a href="test_escape.php">Escape</a></li>
+    	<li><a href="test_escape_quote.php">Escape + Guillemet</a></li>
+    </ul>
+    <!-- // .sideNav -->
+</div>    
+<!-- // #sidebar -->
