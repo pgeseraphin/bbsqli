0acabe81bda9c8bc53b58a172221881bb0ed96c1
pgeseraphin <pgeseraphin@73e4faf7-30dd-42b8-f396-fc12991c0e65>
2011-03-02 10:39:42 +0000

    * Modification des fichiers de paramètres de connexion

--- trunk/ bbsqli/admin/db.php
+++ trunk/ bbsqli/admin/db.php
@@ -1,16 +1,6 @@
 <?php
-
-/*
- * Created on Dec 24, 2010
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
- */
-
-//$conn = new mysqli('localhost', 'root', 'veniu/', 'Blog');
 $conn = new mysqli('localhost', 'root', 'tololo', 'Blog');
 
-/* check connection */
 if (mysqli_connect_errno()) {
 	printf("Connect failed: %s\n", mysqli_connect_error());
 	exit ();
--- trunk/ bbsqli/param_connexion.php
+++ trunk/ bbsqli/param_connexion.php
@@ -1,35 +1,6 @@
 <?php
-/*
- * Created on 24 déc. 2010
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
- */
- /*function accesBase(){
-  $host = "localhost";
-  $user = "root";
-  $pass = "veniu/";
-  $bdd = "Blog";
-  $mysqli = new mysqli($host, $user, $pass, $bdd);
-  if(mysqli_connect_errno()){
-  error_log(mysql_error(), 3, "log/application_log.err");
-  }
-  return $mysqli;
-  
-  }*/
-$host = "localhost";
-$user = "root";
-$pass = "cazeau";
-$bdd = "Blog";
-/*$mysqli = new mysql($host, $user, $pass, $bdd) or die(mysql_error());
- $conn = mysql_connect($host, $user, $pass);
- if(!$conn)
- echo " ...... erreur";
- else
- mysql_select_db($bdd);*/
 $conn = new mysqli('localhost', 'root', 'tololo', 'Blog');
 
-/* check connection */
 if (mysqli_connect_errno()) {
 	printf("Connect failed: %s\n", mysqli_connect_error());
 	exit ();
