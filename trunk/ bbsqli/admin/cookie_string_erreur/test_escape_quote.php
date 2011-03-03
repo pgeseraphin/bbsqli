95e659ce12a7fa5c7b6d3e365a523a749bd82dce
pgeseraphin <pgeseraphin@73e4faf7-30dd-42b8-f396-fc12991c0e65>
2011-03-03 17:33:11 +0000

    * Correction libellé "Cookie String avec erreur"

--- trunk/ bbsqli/admin/cookie_string_erreur/test_escape_quote.php
+++ trunk/ bbsqli/admin/cookie_string_erreur/test_escape_quote.php
@@ -14,7 +14,7 @@
 				<?php require_once 'admin_side.php'; ?>
 					               
                 <!-- h2 stays for breadcrumbs -->
-                <h2><a href="#">COOKIE STRING (sans affichage des erreurs)</a></h2>
+                <h2><a href="#">COOKIE STRING (avec affichage des erreurs)</a></h2>
                 
                 <div id="main">
                 <form action="" class="jNice">
